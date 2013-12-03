0e5342dff46769cb70b1a99d1a42d6eb5ac167e2
Remiii <repository@remibarbe.fr>
2013-12-03 08:45:51 +0100

    Add new post on Primo

--- _project/deployProject.sh
+++ _project/deployProject.sh
@@ -3,11 +3,13 @@
 echo "Start deployProject.sh"
 
 
-echo "Update tags in the blog part"
+echo "Update tags, statistic in the blog part"
 echo "DO NOT USE SUDO FOR THIS CMD!"
 echo "."
 rm -r blog/tag/*
 cp -r _site/blog/tag/* blog/tag/
+#rm blog/statistic/index.html
+#cp _site/blog/statistic/index.html blog/statisitic/index.html
 
 echo "End"
 
