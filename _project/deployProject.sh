88c73c632337acbc169f1dea934b5365f0f1a67d
Remiii <repository@remibarbe.fr>
2013-12-24 17:23:38 +0100

    Hotfix

--- _project/deployProject.sh
+++ _project/deployProject.sh
@@ -8,6 +8,8 @@ echo "DO NOT USE SUDO FOR THIS CMD!"
 echo "."
 rm -r blog/tag/*
 cp -r _site/blog/tag/* blog/tag/
+rm -r api/*
+cp -r _site/api/* api/
 #rm blog/statistic/index.html
 #cp _site/blog/statistic/index.html blog/statisitic/index.html
 
