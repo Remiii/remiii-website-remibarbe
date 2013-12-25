f6124ae177ffe49d391d58b6eb3335996e5b63a6
Remiii <repository@remibarbe.fr>
2013-12-25 22:40:24 +0100

    Add post

---
+++ _posts/2013-12-25-merry-xmas.md
@@ -0,0 +1,11 @@
+---
+layout: post
+title: Merry Xmas
+location: Bordeaux Area, France
+tags:
+    - geek
+    - useless
+---
+
+<script src="https://gist.github.com/Remiii/8121007.js"> </script><br />
+<p>Merry Xmas Everybody!</p>
--- _project/deployProject.sh
+++ _project/deployProject.sh
@@ -6,9 +6,10 @@ echo "Start deployProject.sh"
 echo "Update tags, statistic in the blog part"
 echo "DO NOT USE SUDO FOR THIS CMD!"
 echo "."
-rm -r blog/tag/*
+#rm -r blog/tag/*
+#rm -r api/*
+#jekyll serve --config _config.yml,_config-dev.yml --watch --trace
 cp -r _site/blog/tag/* blog/tag/
-rm -r api/*
 cp -r _site/api/* api/
 #rm blog/statistic/index.html
 #cp _site/blog/statistic/index.html blog/statisitic/index.html
